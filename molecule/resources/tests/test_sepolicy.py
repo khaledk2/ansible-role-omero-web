72f1d9709274b9a032861b55ea65a0c714160fe3
khaledk2 <khaledk2@hotmail.com>
2023-10-25 22:49:51 +0100

    fix

--- molecule/resources/tests/test_sepolicy.py
+++ molecule/resources/tests/test_sepolicy.py
@@ -6,6 +6,6 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-def test_selinuxcustompolicy(host):
+def test_selinux_custom_policy(host):
     out = host.check_output('semodule -l')
     assert 'django' in out
